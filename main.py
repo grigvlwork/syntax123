2f1e2a288dba689113eb77cfeeb212d89af731a0
grigorovich <grigvlwork@gmail.com>
2023-11-24 12:04:25 +0700

    создание проекта

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+import traceback
+import subprocess
+import pyperclip
+import black
+import sys
+import re
+import enchant
+import difflib
+import datetime
+import time
+import shutil
+import os
+import glob
+import qdarkstyle
+from PyQt5 import uic, QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QDialog
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
+from mainwindow import Ui_MainWindow
+from need_file import Ui_need_file_dlg
+import res_rc
