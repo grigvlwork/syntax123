6576c14a010b2796a2e030fb4e4c94da26394e81
grigorovich <grigvlwork@gmail.com>
2023-11-24 14:27:48 +0700

    добавляю логику

---
+++ classes.py
@@ -0,0 +1,46 @@
+class Part:
+    def __init__(self, number=0, code='', explanation=''):
+        self.number = number
+        self.code = code
+        self.explanation = explanation
+        self.checked = False
+
+
+class Task:
+    def __init__(self):
+        self.tasks = []
+        self.comment = ''
+
+    def parse(self, text):
+        i = 1
+        while f'<code{i}>' in text:
+            begin = text.find(f'<code{i}>') + len(f'<code{i}>')
+            end = text.find(f'</code{i}>')
+            code = text[begin:end]
+            if f'<explanation{i}>' in text:
+                begin = text.find(f'<explanation{i}>') + len(f'<explanation{i}>')
+                end = text.find(f'</explanation{i}>')
+                explanation = text[begin:end]
+            else:
+                explanation = ''
+            self.tasks.append(Part(i, code, explanation))
+            i += 1
+        if '<comment>' in text:
+            begin = text.find('<comment>') + len('<comment>')
+            end = text.find('</comment>')
+            self.comment = text[begin:end]
+
+    def get_text(self):
+        text = ''
+        for i in range(len(self.tasks)):
+            text += f'<code{i + 1}>\n{self.tasks[i].code}\n</code{i + 1}>' + \
+                    f'\n<explanation{i + 1}>\n{self.tasks[i].explanation}\n</explanation{i + 1}>\n'
+        text += f'<comment>\n{self.comment}\n</comment>'
+        return text
+
+    def is_ready(self):
+        for t in self.tasks:
+            if not t.checked:
+                return False
+        return True
+
--- main.py
+++ main.py
@@ -15,6 +15,148 @@ import glob
 import qdarkstyle
 from PyQt5 import uic, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QDialog
+from PyQt5 import QtGui
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
 from mainwindow import Ui_MainWindow
 import res_rc
+from classes import Task
+
+
+def run_text(text, timeout):
+    with open('code.py', 'w', encoding='utf-8') as c:
+        c.write(text)
+    try:
+        completed_process = subprocess.run(['python', 'code.py'], capture_output=True, text=True, timeout=timeout)
+        if completed_process.returncode == 0:
+            t = completed_process.stdout
+            t = t.encode('cp1251').decode('utf-8')
+            if len(t) > 25:
+                return t[:25] + '..'
+            else:
+                return t
+        else:
+            t = completed_process.stderr
+            t = t.encode('cp1251').decode('utf-8')
+            if len(t) > 50:
+                return t[:50] + '\n' + t[50:]
+            else:
+                return t
+    except subprocess.TimeoutExpired:
+        return f'Программа выполнялась более {timeout} секунд'
+
+
+def remove_comments(code):
+    return re.sub(r'#.*', '', code)
+
+
+def spell_check(text):
+    rus_alph = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    words = []
+    word = ''
+    for c in text:
+        if c.lower() in rus_alph:
+            word += c
+        else:
+            if len(word) > 0:
+                words.append(word)
+                word = ''
+    result = []
+    dictionary = enchant.Dict("ru_RU")
+    for w in words:
+        if not dictionary.check(w):
+            sim = dict()
+            suggestions = set(dictionary.suggest(w))
+            for word in suggestions:
+                measure = difflib.SequenceMatcher(None, w, word).ratio()
+                sim[measure] = word
+            result.append([w, sim[max(sim.keys())]])
+    return result
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.current_part = 1
+        self.part_buttons = [
+            self.task1_btn, self.task2_btn, self.task3_btn, self.task4_btn,
+            self.task5_btn, self.task6_btn, self.task7_btn, self.task8_btn,
+            self.task9_btn, self.task10_btn
+        ]
+        self.amount_buttons = 10
+        self.task = Task()
+        self.clear_controls()
+        self.toggle_theme_btn.clicked.connect(self.change_theme)
+        self.corrected_cb.clicked.connect(self.mark_part_checked)
+        self.insert_answer_btn.clicked.connect(self.insert)
+
+
+    def change_theme(self):
+        if self.toggle_theme_btn.text() == 'Светлая тема':
+            self.toggle_theme_btn.setText('Тёмная тема')
+            app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5', palette=qdarkstyle.LightPalette))
+        else:
+            self.toggle_theme_btn.setText('Светлая тема')
+            app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5', palette=qdarkstyle.DarkPalette))
+
+    def change_icon(self, button, checked):
+        icon = QtGui.QIcon()
+        if checked:
+            icon.addPixmap(QtGui.QPixmap(":/img/check_green.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        else:
+            icon.addPixmap(QtGui.QPixmap(":/img/check.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        button_to_change = self.part_buttons[button - 1]
+        button_to_change.setIcon(icon)
+
+    def mark_part_checked(self):
+        self.change_icon(self.current_part, self.corrected_cb.isChecked())
+
+    def clear_controls(self):
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/img/check.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        for i in range(1, self.amount_buttons):
+            self.change_icon(i, False)
+            if i > 0:
+                self.part_buttons[i].setVisible(False)
+
+    def insert(self):
+        s = pyperclip.paste()
+        self.teacher_answer_pte.setPlainText(s)
+        self.processing()
+
+    def processing(self):
+        t = self.teacher_answer_pte.toPlainText()
+        self.task.parse(t)
+        self.explanation_pte.clear()
+        self.explanation_pte.appendPlainText(self.task.tasks[0].explanation)
+        self.correct_code_pte.clear()
+        self.correct_code_pte.appendPlainText(self.task.tasks[0].code)
+        self.my_answer_pte.clear()
+        self.my_answer_pte.appendPlainText(self.task.get_text())
+        # self.create_my_answer()
+
+
+
+
+
+
+
+
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    print(tb)
+    msg = QMessageBox.critical(
+        None,
+        "Error catched!:",
+        tb
+    )
+    QApplication.quit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    sys.excepthook = excepthook
+    app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5', palette=qdarkstyle.DarkPalette))
+    ex.show()
+    sys.exit(app.exec_())
